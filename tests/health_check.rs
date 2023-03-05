bae2230b7fba4306601e55bb6446b020ebdd7a65
phantie <funtom1605@gmail.com>
2023-03-05 13:47:23 +0200

    First test working

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,22 +2,21 @@ use api_aga_in;
 
 #[tokio::test]
 async fn health_check_works() {
-    // Arrange
-    spawn_app().await.expect("Failed to spawn our app.");
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
+    spawn_app();
+
     let client = reqwest::Client::new();
-    // Act
     let response = client
         .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execute request.");
-    // Assert
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-// Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
+
+fn spawn_app() {
+    let server = api_aga_in::run();
+
+    let _ = tokio::spawn(server);
 }
