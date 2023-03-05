27eb9b7939f8bdb84a8ade6a107a8b63f09ce642
phantie <funtom1605@gmail.com>
2023-03-05 13:38:50 +0200

    Restructure

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
-use axum::{http::StatusCode, response::Html, routing::get, Router};
-use std::net::SocketAddr;
+use api_aga_in::run;
 
 #[tokio::main]
 async fn main() {
-    #[rustfmt::skip]
-    let app =
-        Router::new()
-        .route("/", get(index))
-        .route("/health_check", get(health_check))
-    ;
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-async fn index() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
-}
-
-async fn health_check() -> StatusCode {
-    StatusCode::OK
+    run().await
 }
