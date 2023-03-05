6aec996b40c19c28aa7b355e767f29c8d0451c43
phantie <funtom1605@gmail.com>
2023-03-05 12:51:23 +0200

    Hello world index endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    #[rustfmt::skip]
+    let app =
+        Router::new()
+        .route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
