275a8f7173386dca3493236105f43d533b9dcbd4
phantie <funtom1605@gmail.com>
2023-03-07 13:36:53 +0200

    Db with state

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,6 +25,15 @@ fn spawn_app() -> String {
     format!("http://127.0.0.1:{}", port)
 }
 
+fn spawn_app_v2() -> String {
+    // trying to bind port 0 will trigger an OS scan for an available port
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener);
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
