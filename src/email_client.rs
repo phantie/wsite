fe4912d7ccaadf767449fb562777b6b749b491bd
phantie <funtom1605@gmail.com>
2023-03-13 12:05:36 +0200

    EmailClient testing: send_email_fails_if_the_server_returns_500

--- src/email_client.rs
+++ src/email_client.rs
@@ -49,7 +49,7 @@ impl EmailClient {
             )
             .json(&request_body);
 
-        request.send().await?;
+        request.send().await?.error_for_status()?;
 
         Ok(())
     }
@@ -69,7 +69,7 @@ struct SendEmailRequest<'a> {
 mod tests {
     use crate::domain::SubscriberEmail;
     use crate::email_client::EmailClient;
-    use claim::assert_ok;
+    use claim::{assert_err, assert_ok};
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
@@ -140,5 +140,26 @@ mod tests {
             .await;
 
         assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        assert_err!(outcome);
     }
 }
