d71333a0762c6e67308d4c3e2d499acca139195d
phantie <funtom1605@gmail.com>
2023-03-15 15:55:48 +0200

    debug APP_APPLICATION__BASE_URL env var in prod

--- src/configuration.rs
+++ src/configuration.rs
@@ -57,6 +57,9 @@ pub fn get_configuration() -> Settings {
         .add_source(config::Environment::with_prefix("app").separator("__"))
         .build();
 
+    let var = std::env::var("APP_APPLICATION__BASE_URL");
+    dbg!(var);
+
     config.unwrap().try_deserialize().unwrap()
 }
 
