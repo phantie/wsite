4095c11979b88ac5ab0534bbee03de1fc60e3353
phantie <funtom1605@gmail.com>
2023-03-17 17:35:33 +0200

    basic auth with raw password storage

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,12 +1,12 @@
 use crate::database::*;
 use crate::startup::AppState;
-use axum::body::{Bytes, Full};
+use axum::body::{Bytes, Empty, Full};
 use axum::extract::rejection::TypedHeaderRejection;
 use axum::extract::{Json, State, TypedHeader};
 use axum::headers::{authorization::Basic, Authorization};
 use axum::http::StatusCode;
-use axum::response::Response;
-use secrecy::Secret;
+use axum::response::{IntoResponse, Response};
+use secrecy::{ExposeSecret, Secret};
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -25,18 +25,25 @@ pub async fn publish_newsletter(
     State(state): State<AppState>,
     maybe_basic_auth: Result<TypedHeader<Authorization<Basic>>, TypedHeaderRejection>,
     Json(body): Json<BodyData>,
-) -> Response<Full<Bytes>> {
+) -> Response<Empty<Bytes>> {
+    let unauthorized = Response::builder()
+        .status(StatusCode::UNAUTHORIZED)
+        .header("WWW-Authenticate", r#"Basic realm="publish""#)
+        .body(Empty::new())
+        .unwrap();
+
     let basic_auth = match maybe_basic_auth {
         Ok(TypedHeader(basic_auth)) => basic_auth,
-        Err(_) => {
-            return Response::builder()
-                .status(StatusCode::UNAUTHORIZED)
-                .header("WWW-Authenticate", r#"Basic realm="publish""#)
-                .body(Full::from(""))
-                .unwrap()
-        }
+        Err(_) => return unauthorized,
     };
-    let _credentials: Credentials = basic_auth.into();
+
+    let credentials: Credentials = basic_auth.into();
+    let user_id = validate_credentials(&state, &credentials).await;
+
+    let user_id = match user_id {
+        None => return unauthorized,
+        Some(user_id) => user_id,
+    };
 
     let subscriptions_docs = Subscription::all_async(&state.database.collections.subscriptions)
         .await
@@ -61,7 +68,7 @@ pub async fn publish_newsletter(
 
     Response::builder()
         .status(StatusCode::OK)
-        .body(Full::from(""))
+        .body(Empty::new())
         .unwrap()
 }
 
@@ -76,5 +83,21 @@ impl From<Authorization<Basic>> for Credentials {
             username: value.username().into(),
             password: Secret::new(value.password().into()),
         }
+    }
+}
+
+async fn validate_credentials(state: &AppState, credentials: &Credentials) -> Option<u64> {
+    let user_docs = User::all_async(&state.database.collections.users)
+        .await
+        .unwrap();
+
+    let user = user_docs.into_iter().find(|doc| {
+        doc.contents.username == credentials.username
+            && doc.contents.password == *credentials.password.expose_secret()
+    });
+
+    match user {
+        None => None,
+        Some(user) => Some(user.header.id),
     }
 }
