223526e8381c022e35799cb897524697790dd2ff
phantie <funtom1605@gmail.com>
2023-03-20 05:04:21 +0200

    error handling and tracing for admin_dashboard

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,4 +1,5 @@
 use crate::{database::*, startup::AppState};
+use anyhow::Context;
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
@@ -6,22 +7,56 @@ use axum::{
 };
 use axum_sessions::extractors::ReadableSession;
 
-pub async fn admin_dashboard(State(state): State<AppState>, session: ReadableSession) -> Response {
+#[tracing::instrument(
+    skip(state, session),
+    fields(user_id=tracing::field::Empty)
+)]
+#[axum_macros::debug_handler]
+pub async fn admin_dashboard(
+    State(state): State<AppState>,
+    session: ReadableSession,
+) -> Result<Response, DashboardError> {
     let user_id: Option<u64> = session.get("user_id");
 
     match user_id {
-        None => Redirect::to("/login").into_response(),
+        None => Ok(Redirect::to("/login").into_response()),
         Some(id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&id));
             let user = User::get_async(id, &state.database.collections.users)
                 .await
-                .unwrap()
-                .unwrap();
+                .context("Failed to fetch user by id")
+                .map_err(DashboardError::UnexpectedError)?
+                .context("No user found by the id")
+                .map_err(DashboardError::AuthError)?;
 
-            (
+            Ok((
                 StatusCode::OK,
                 format!("Welcome {}", user.contents.username),
             )
-                .into_response()
+                .into_response())
         }
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum DashboardError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl axum::response::IntoResponse for DashboardError {
+    fn into_response(self) -> axum::response::Response {
+        let (trace_message, response) = match &self {
+            Self::AuthError(_e) => (self.to_string(), Redirect::to("/login").into_response()),
+            Self::UnexpectedError(e) => (
+                format!("{}: {}", self.to_string(), e.source().unwrap()),
+                StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            ),
+        };
+        tracing::error!("{}", trace_message);
+        response
     }
 }
