bcc383f3b15ae544748b83a49d7ac3f9a66fa1f7
phantie <funtom1605@gmail.com>
2023-04-11 18:17:53 +0300

    AdminDashboard to struct component

--- frontend/src/components/admin/dashboard/dashboard.rs
+++ frontend/src/components/admin/dashboard/dashboard.rs
@@ -2,22 +2,34 @@ use crate::components::imports::*;
 
 use super::WelcomeMessage;
 
-#[styled_component]
-pub fn AdminDashboard() -> Html {
-    html! {
-        <>
-            <h1><WelcomeMessage/></h1>
-            <p>{ "Available actions:" }</p>
-            <ol>
-                <li>
-                    <a href={ routes().root.admin.password.get().complete().to_owned() }>{ "Change password" }</a>
-                </li>
-                <li>
-                    <form name="logoutForm" action="/api/admin/logout" method="post">
-                        <input type="submit" value="Logout"/>
-                    </form>
-                </li>
-            </ol>
-        </>
+pub struct AdminDashboard;
+
+pub enum AdminDashboardMsg {}
+
+impl Component for AdminDashboard {
+    type Message = AdminDashboardMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <h1><WelcomeMessage/></h1>
+                <p>{ "Available actions:" }</p>
+                <ol>
+                    <li>
+                        <a href={ routes().root.admin.password.get().complete().to_owned() }>{ "Change password" }</a>
+                    </li>
+                    <li>
+                        <form name="logoutForm" action="/api/admin/logout" method="post">
+                            <input type="submit" value="Logout"/>
+                        </form>
+                    </li>
+                </ol>
+            </>
+        }
     }
 }
