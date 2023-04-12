278e3eb652b03b8d110b5f0458dc59835355aad2
phantie <funtom1605@gmail.com>
2023-04-12 16:02:39 +0300

    remove old frontend enpoint

--- backend/src/routes/home/mod.rs
+++
@@ -1,6 +0,0 @@
-use crate::routes::imports::*;
-
-#[allow(dead_code)]
-pub async fn home() -> Html<&'static str> {
-    Html(include_str!("home.html"))
-}
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -2,14 +2,12 @@ mod imports;
 
 mod admin;
 mod health_check;
-mod home;
 mod login;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 pub use admin::*;
 pub use health_check::*;
-pub use home::*;
 pub use login::*;
 pub use newsletters::*;
 pub use subscriptions::*;
