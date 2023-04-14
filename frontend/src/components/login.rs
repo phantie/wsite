44e0824941bc3519b0a1aef8a5d1078e27b7bd32
phantie <funtom1605@gmail.com>
2023-04-14 17:39:35 +0300

    login comp to struct comp

--- frontend/src/components/login.rs
+++ frontend/src/components/login.rs
@@ -1,93 +1,108 @@
 use crate::components::imports::*;
 use interfacing::LoginForm;
 
-#[styled_component]
-pub fn Login() -> Html {
-    let username_ref = use_node_ref();
-    let password_ref = use_node_ref();
+pub struct Login {
+    username_ref: NodeRef,
+    password_ref: NodeRef,
+}
 
-    let navigator = use_navigator().unwrap();
-    let location = use_location().unwrap();
-    let query_params = location.query::<HashMap<String, String>>().unwrap();
+impl Component for Login {
+    type Message = ();
+    type Properties = ();
 
-    let error_node = match query_params.get("error") {
-        None => html! {},
-        Some(error) => html! {
-            <div class="alert alert-warning" role="alert">
-                { error }
-            </div>
-        },
-    };
+    #[allow(unused_variables)]
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            username_ref: NodeRef::default(),
+            password_ref: NodeRef::default(),
+        }
+    }
 
-    let onsubmit = {
-        let username_ref = username_ref.clone();
-        let password_ref = password_ref.clone();
+    #[allow(unused_variables)]
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let username_ref = self.username_ref.clone();
+        let password_ref = self.password_ref.clone();
 
-        Callback::from(move |event: SubmitEvent| {
-            event.prevent_default();
-            let window = web_sys::window().unwrap();
-            let navigator = navigator.clone();
+        let navigator = ctx.link().navigator().unwrap();
+        let location = ctx.link().location().unwrap();
+        let query_params = location.query::<HashMap<String, String>>().unwrap();
 
-            let username_field = username_ref.cast::<HtmlInputElement>().unwrap();
-            let password_field = password_ref.cast::<HtmlInputElement>().unwrap();
+        let error_node = match query_params.get("error") {
+            None => html! {},
+            Some(error) => html! {
+                <div class="alert alert-warning" role="alert">
+                    { error }
+                </div>
+            },
+        };
 
-            let login_form = LoginForm {
-                username: username_field.value(),
-                password: SecretString::new(password_field.value()),
-            };
+        let onsubmit = {
+            let username_ref = username_ref.clone();
+            let password_ref = password_ref.clone();
 
-            wasm_bindgen_futures::spawn_local(async move {
-                console::log!(format!("submitting: {:?}", login_form));
+            Callback::from(move |event: SubmitEvent| {
+                event.prevent_default();
+                let window = web_sys::window().unwrap();
+                let navigator = navigator.clone();
 
-                let login_response = request_login(&login_form).await.unwrap();
+                let username_field = username_ref.cast::<HtmlInputElement>().unwrap();
+                let password_field = password_ref.cast::<HtmlInputElement>().unwrap();
 
-                console_log_status(&login_response);
-
-                match login_response.status() {
-                    200 => {
-                        navigator.push(&Route::AdminDashboard);
-                    }
-                    401 => {
-                        password_field.set_value("");
-                        window.alert_with_message("Unauthorized").unwrap();
-                    }
-                    _ => unreachable!(),
+                let login_form = LoginForm {
+                    username: username_field.value(),
+                    password: SecretString::new(password_field.value()),
                 };
+
+                wasm_bindgen_futures::spawn_local(async move {
+                    console::log!(format!("submitting: {:?}", login_form));
+
+                    let login_response = request_login(&login_form).await.unwrap();
+
+                    console_log_status(&login_response);
+
+                    match login_response.status() {
+                        200 => {
+                            navigator.push(&Route::AdminDashboard);
+                        }
+                        401 => {
+                            password_field.set_value("");
+                            window.alert_with_message("Unauthorized").unwrap();
+                        }
+                        _ => unreachable!(),
+                    };
+                })
             })
-        })
-    };
+        };
 
-    html! {
-        <>
-            <Global css={css!("
-                display: flex;
-                justify-content: center;
-            ")}/>
+        html! {
+            <>
+                <Global css={ "display: flex; justify-content: center;" }/>
 
-            <h1 class={css!{"padding-top: 20px; padding-bottom: 20px;"}}>{ "Login" }</h1>
+                <h1 class={ css!{"padding-top: 20px; padding-bottom: 20px;"} }>{ "Login" }</h1>
 
-            { error_node }
+                { error_node }
 
-            <div>
-                <form class={css!("width: 450px; max-width: 90vw;")}{onsubmit} method="post">
-                    <div class="form-group">
-                        <label class="col-form-label-lg" for="username_input">{ "Username" }</label>
-                        <input ref={username_ref} type="text" placeholder="Enter Username"
-                        name="username" id="username_input" class="form-control form-control-lg"
-                        required={true}/>
-                    </div>
+                <div>
+                    <form class={ css!{"width: 450px; max-width: 90vw;"} }{ onsubmit } method="post">
+                        <div class="form-group">
+                            <label class="col-form-label-lg" for="username_input">{ "Username" }</label>
+                            <input ref={username_ref} type="text" placeholder="Enter Username"
+                            name="username" id="username_input" class="form-control form-control-lg"
+                            required={true}/>
+                        </div>
 
-                    <div class="form-group">
-                        <label class="col-form-label-lg" for="password_input">{ "Password" }</label>
-                        <input ref={password_ref} type="password" placeholder="Enter Password"
-                        name="password" id="password_input" class="form-control form-control-lg"
-                        required={true}/>
-                    </div>
+                        <div class="form-group">
+                            <label class="col-form-label-lg" for="password_input">{ "Password" }</label>
+                            <input ref={password_ref} type="password" placeholder="Enter Password"
+                            name="password" id="password_input" class="form-control form-control-lg"
+                            required={true}/>
+                        </div>
 
-                    <button type="submit" class="btn btn-dark btn-lg">{ "Login" }</button>
-                </form>
-            </div>
-        </>
+                        <button type="submit" class="btn btn-dark btn-lg">{ "Login" }</button>
+                    </form>
+                </div>
+            </>
+        }
     }
 }
 
