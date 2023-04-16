fe911c455e01c9b5555935629f70c495136e05bf
phantie <funtom1605@gmail.com>
2023-04-16 17:08:12 +0300

    refactor user session as context

--- common/interfacing/src/admin_session.rs
+++ common/interfacing/src/admin_session.rs
@@ -1,6 +1,6 @@
 use crate::imports::*;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct AdminSession {
     pub user_id: u64,
     pub username: String,
--- frontend/src/components/admin/dashboard/dashboard.rs
+++ frontend/src/components/admin/dashboard/dashboard.rs
@@ -1,61 +1,25 @@
 use crate::components::imports::*;
 
+use super::super::WithAuth;
 use super::Logout;
 use super::WelcomeMessage;
 
-pub struct Dashboard {
-    session: Session,
-}
-
-enum Session {
-    Unloaded,
-    Loaded(interfacing::AdminSession),
-    Error(SessionError),
-}
-
-impl Session {
-    fn is_unexpected_error(&self) -> bool {
-        match self {
-            Self::Error(e) => e.is_unexpected(),
-            _ => false,
-        }
-    }
-}
-
-pub enum Msg {
-    SessionLoaded(interfacing::AdminSession),
-    SessionError(SessionError),
-}
+pub struct Dashboard;
 
 impl Component for Dashboard {
-    type Message = Msg;
+    type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            session: Session::Unloaded,
-        }
+        Self
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        if self.session.is_unexpected_error() {
-            return internal_problems();
-        }
-
-        let username: Option<AttrValue> = match &self.session {
-            Session::Unloaded => None,
-            Session::Loaded(session) => Some(session.username.clone().into()),
-            Session::Error(SessionError::AuthError) => {
-                unreachable!("because you are redirected to login")
-            }
-            Session::Error(_) => {
-                unreachable!("because of self.session.is_unexpected_error() guard")
-            }
-        };
+        console::log!("drawing Dashboard");
 
         html! {
-            <>
-                <h1><WelcomeMessage {username}/></h1>
+            <WithAuth>
+                <h1><WelcomeMessage/></h1>
                 <p>{ "Available actions:" }</p>
                 <ol>
                     <li>
@@ -69,43 +33,7 @@ impl Component for Dashboard {
                         <Logout/>
                     </li>
                 </ol>
-            </>
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        let navigator = ctx.link().navigator().unwrap();
-        match msg {
-            Self::Message::SessionLoaded(session) => {
-                self.session = Session::Loaded(session);
-                true
-            }
-            Self::Message::SessionError(e @ SessionError::AuthError) => {
-                self.session = Session::Error(e);
-                console::log!(format!("message SessionMissing from Dashboard"));
-                navigator
-                    .push_with_query(
-                        &Route::Login,
-                        &HashMap::from([("error", "Login to access dashboard")]),
-                    )
-                    .unwrap();
-                false
-            }
-            Self::Message::SessionError(e) => {
-                self.session = Session::Error(e);
-                true
-            }
-        }
-    }
-
-    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
-        if first_render {
-            ctx.link().send_future(async {
-                match fetch_admin_session().await {
-                    Ok(session) => Self::Message::SessionLoaded(session),
-                    Err(e) => Self::Message::SessionError(e),
-                }
-            });
+            </WithAuth>
         }
     }
 }
