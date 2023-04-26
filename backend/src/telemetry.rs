23b97f10908165b1ba62bccbc3373627d456209d
phantie <funtom1605@gmail.com>
2023-04-27 01:34:48 +0300

    rewrite tracing

--- backend/src/telemetry.rs
+++ backend/src/telemetry.rs
@@ -1,33 +1,72 @@
-use tracing::subscriber::set_global_default;
-use tracing::Subscriber;
+use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::fmt::MakeWriter;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{
+    filter,
+    fmt::MakeWriter,
+    layer::{Layer, SubscriberExt},
+    EnvFilter, Registry,
+};
 
-pub fn get_subscriber<Sink>(
+static BACKEND_CRATE_NAME: &str = "api_aga_in";
+
+pub struct TracingSubscriber {
     name: String,
     env_filter: String,
-    sink: Sink,
-) -> impl Subscriber + Send + Sync
-where
-    // for details https://doc.rust-lang.org/nomicon/hrtb.html
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, sink);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
 }
 
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+impl TracingSubscriber {
+    pub fn new<T>(name: T) -> Self
+    where
+        T: AsRef<str>,
+    {
+        Self {
+            name: name.as_ref().to_string(),
+            env_filter: "info".into(),
+        }
+    }
+
+    /// Creates a [`tracing::Subscriber`] configured to format logs with [`Bunyan`]
+    ///
+    /// [`Bunyan`]: https://docs.rs/tracing-bunyan-formatter/latest/tracing_bunyan_formatter/
+    pub fn build<Sink>(self, sink: Sink) -> impl Subscriber + Sync + Send
+    where
+        Sink: for<'a> MakeWriter<'a> + Sync + Send + 'static,
+    {
+        let logging_layer = {
+            let skip_fields = ["file", "line", "target"];
+
+            let formatting_layer = BunyanFormattingLayer::new(self.name.clone(), sink)
+                .skip_fields(skip_fields.into_iter().map(|s| s.to_owned()).into_iter())
+                .expect("unable to build the bunyan formatting layer");
+
+            let env_filter = EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| EnvFilter::new(self.env_filter));
+
+            let target_filter = filter::Targets::new()
+                .with_target(BACKEND_CRATE_NAME, tracing::Level::INFO)
+                .with_target("tower_http::trace", tracing::Level::INFO)
+                .with_target("mio::poll", filter::LevelFilter::OFF);
+            formatting_layer
+                .with_filter(target_filter)
+                .with_filter(env_filter)
+
+            // formatting_layer
+        };
+
+        Registry::default()
+            .with(JsonStorageLayer)
+            .with(logging_layer)
+    }
+}
+
+/// Sets `subscriber` as the global default [`tracing::Subscriber`].
+pub fn init_global_default(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
 
+/// Spawns a blocking task in the scope of the current tracing span.
 pub fn spawn_blocking_with_tracing<F, R>(f: F) -> tokio::task::JoinHandle<R>
 where
     F: FnOnce() -> R + Send + 'static,
