f6048fab7f6bcd9c81c93247ec22a20e526dc3fb
phantie <funtom1605@gmail.com>
2023-05-02 06:43:37 +0300

    try to fix hanging client after long idling

--- backend/src/routes/shape.rs
+++ backend/src/routes/shape.rs
@@ -2,11 +2,39 @@ use crate::routes::imports::*;
 use remote_database::shema::Shape;
 
 #[axum_macros::debug_handler]
-pub async fn all_shapes(State(state): State<AppState>) -> Json<Vec<Shape>> {
+#[allow(unused_mut)]
+pub async fn all_shapes(State(mut state): State<AppState>) -> Response {
+    // // Original solution to hanging client replaced by perpetural database pinging
+    // let shapes = state.remote_database.collections.shapes.clone();
+    // let mut retried = false;
+    // loop {
+    //     match tokio::time::timeout_at(
+    //         tokio::time::Instant::now() + std::time::Duration::from_secs(5),
+    //         Shape::all_async(&shapes),
+    //     )
+    //     .await
+    //     {
+    //         Ok(docs) => {
+    //             let docs = docs.unwrap();
+    //             let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
+    //             return Json(contents).into_response();
+    //         }
+    //         Err(_) => {
+    //             if retried {
+    //                 return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    //             } else {
+    //                 state.remote_database.reconfigure().await;
+    //                 retried = true;
+    //                 continue;
+    //             }
+    //         }
+    //     }
+    // }
+
     let shapes = state.remote_database.collections.shapes;
     let docs = Shape::all_async(&shapes).await.unwrap();
     let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
-    Json(contents)
+    Json(contents).into_response()
 }
 
 #[axum_macros::debug_handler]
