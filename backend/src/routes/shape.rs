c4b9331109297e5402d7e9e41d1115a974ab1865
phantie <funtom1605@gmail.com>
2023-05-02 08:05:58 +0300

    impl ping for Client

--- backend/src/routes/shape.rs
+++ backend/src/routes/shape.rs
@@ -4,37 +4,39 @@ use remote_database::shema::Shape;
 #[axum_macros::debug_handler]
 #[allow(unused_mut)]
 pub async fn all_shapes(State(mut state): State<AppState>) -> Response {
-    // // Original solution to hanging client replaced by perpetural database pinging
-    // let shapes = state.remote_database.collections.shapes.clone();
-    // let mut retried = false;
-    // loop {
-    //     match tokio::time::timeout_at(
-    //         tokio::time::Instant::now() + std::time::Duration::from_secs(5),
-    //         Shape::all_async(&shapes),
-    //     )
-    //     .await
-    //     {
-    //         Ok(docs) => {
-    //             let docs = docs.unwrap();
-    //             let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
-    //             return Json(contents).into_response();
-    //         }
-    //         Err(_) => {
-    //             if retried {
-    //                 return StatusCode::INTERNAL_SERVER_ERROR.into_response();
-    //             } else {
-    //                 state.remote_database.reconfigure().await;
-    //                 retried = true;
-    //                 continue;
-    //             }
-    //         }
-    //     }
-    // }
+    // Original solution to hanging client replaced by perpetural database pinging
+    // Also a solution to any connection teardown
+    // for example, to restore connection with a restarted database server
+    let mut retried = false;
+    loop {
+        let shapes = state.remote_database.collections.shapes.clone();
+        match tokio::time::timeout_at(
+            tokio::time::Instant::now() + std::time::Duration::from_secs(2),
+            Shape::all_async(&shapes),
+        )
+        .await
+        {
+            Ok(docs) => {
+                let docs = docs.unwrap();
+                let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
+                return Json(contents).into_response();
+            }
+            Err(_) => {
+                if retried {
+                    return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+                } else {
+                    state.remote_database.reconfigure().await;
+                    retried = true;
+                    continue;
+                }
+            }
+        }
+    }
 
-    let shapes = state.remote_database.collections.shapes;
-    let docs = Shape::all_async(&shapes).await.unwrap();
-    let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
-    Json(contents).into_response()
+    // let shapes = state.remote_database.collections.shapes;
+    // let docs = Shape::all_async(&shapes).await.unwrap();
+    // let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
+    // Json(contents).into_response()
 }
 
 #[axum_macros::debug_handler]
