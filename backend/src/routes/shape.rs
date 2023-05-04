8a7c7812050a6c7341aa6d232d7283f6ce459ca3
phantie <funtom1605@gmail.com>
2023-05-04 14:47:32 +0300

    ApiError

--- backend/src/routes/shape.rs
+++ backend/src/routes/shape.rs
@@ -17,14 +17,13 @@ impl Default for HangingStrategy {
 }
 
 impl HangingStrategy {
-    // Original solution to hanging client replaced by perpetural database pinging
-    // Also a solution to any connection teardown
-    // for example, to restore connection with a restarted database server
+    // Attempt to renew connection with the database server if it hangs
+    // because there's no timeouts on external API calls to it
     async fn execute<F, C, R>(
         self,
         closure: C,
         shared_database: SharedRemoteDatabase,
-    ) -> Result<R, ()>
+    ) -> Result<R, ApiError>
     where
         F: std::future::Future<Output = R>,
         C: Fn() -> F,
@@ -43,7 +42,8 @@ impl HangingStrategy {
                         Ok(r) => return Ok(r),
                         Err(_elapsed) => {
                             if retried_times >= max_times {
-                                return Err(());
+                                tracing::info!("FUCKING ERROR");
+                                return Err(ApiError::DatabaseHangs);
                             }
 
                             shared_database.write().await.reconfigure().await;
@@ -59,25 +59,20 @@ impl HangingStrategy {
 #[axum_macros::debug_handler]
 pub async fn all_shapes(
     Extension(shared_database): Extension<SharedRemoteDatabase>,
-) -> impl IntoResponse {
+) -> Result<Json<Vec<Shape>>, ApiError> {
     tracing::info!("Remote database ID: {}", shared_database.read().await.id);
 
+    // BUG after shared database is updated it still uses the old ref, &shapes in this case
     let shapes = shared_database.read().await.collections.shapes.clone();
 
     let docs = HangingStrategy::default()
         .execute(|| Shape::all_async(&shapes), shared_database.clone())
-        .await
-        .expect("failed to unhang")
+        .await?
         .expect("failed to fetch valid data");
 
     let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
 
-    Json(contents)
-
-    // let shapes = state.remote_database.collections.shapes;
-    // let docs = Shape::all_async(&shapes).await.unwrap();
-    // let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
-    // Json(contents).into_response()
+    Ok(Json(contents))
 }
 
 #[axum_macros::debug_handler]
@@ -88,4 +83,41 @@ pub async fn new_shape(
     let shapes = shared_database.read().await.collections.shapes.clone();
     body.push_into_async(&shapes).await.unwrap();
     StatusCode::OK.into_response()
+}
+
+#[derive(thiserror::Error, Debug)]
+#[allow(dead_code)]
+pub enum ApiError {
+    #[error("Json is rejected")]
+    JsonRejection(#[from] JsonRejection),
+
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+
+    #[error("Database hangs")]
+    DatabaseHangs,
+
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        let trace_message = match &self {
+            Self::JsonRejection(rejection) => {
+                format!("{}: {}", self.to_string(), rejection.to_string())
+            }
+            Self::UnexpectedError(e) => format!("{}: {}", self.to_string(), e.source().unwrap()),
+            _ => self.to_string(),
+        };
+        tracing::error!("{}", trace_message);
+
+        match &self {
+            Self::JsonRejection(_e) => StatusCode::BAD_REQUEST,
+            Self::AuthError(_e) => StatusCode::UNAUTHORIZED,
+            Self::UnexpectedError(_e) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::DatabaseHangs => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+        .into_response()
+    }
 }
