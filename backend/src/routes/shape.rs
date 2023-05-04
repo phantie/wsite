24646c386b24a0aba2b3ec14ddcd76a9ebbb146d
phantie <funtom1605@gmail.com>
2023-05-04 11:30:33 +0300

    generalized one more robust fetch strategy

--- backend/src/routes/shape.rs
+++ backend/src/routes/shape.rs
@@ -2,65 +2,51 @@ use crate::routes::imports::*;
 use remote_database::shema::Shape;
 
 #[axum_macros::debug_handler]
-#[allow(unused_mut)]
 pub async fn all_shapes(Extension(shared_database): Extension<SharedRemoteDatabase>) -> Response {
-    // async fn retry_maybe<F>(future: F, remote_database: &mut RemoteDatabase) -> Result<(), ()>
-    // where
-    //     F: std::future::Future + Clone,
-    // {
-    //     let mut retried_times = 0;
-
-    //     loop {
-    //         match tokio::time::timeout_at(
-    //             tokio::time::Instant::now() + std::time::Duration::from_secs(2),
-    //             future.clone(),
-    //         )
-    //         .await
-    //         {
-    //             Ok(_) => return Ok(()),
-    //             Err(_) => {
-    //                 if retried_times >= 1 {
-    //                     return Err(());
-    //                 }
-
-    //                 remote_database.reconfigure().await;
-    //                 retried_times += 1;
-    //             }
-    //         }
-    //     }
-    // }
-
-    tracing::info!("Remote database ID: {}", shared_database.read().await.id);
-
     // Original solution to hanging client replaced by perpetural database pinging
     // Also a solution to any connection teardown
     // for example, to restore connection with a restarted database server
-    let mut retried = false;
-    loop {
-        let shapes = shared_database.read().await.collections.shapes.clone();
+    async fn retry_maybe<F, C, R>(
+        closure: C,
+        shared_database: SharedRemoteDatabase,
+    ) -> Result<R, ()>
+    where
+        F: std::future::Future<Output = R>,
+        C: Fn() -> F,
+    {
+        let mut retried_times = 0;
 
-        match tokio::time::timeout_at(
-            tokio::time::Instant::now() + std::time::Duration::from_secs(2),
-            Shape::all_async(&shapes),
-        )
-        .await
-        {
-            Ok(docs) => {
-                let docs = docs.unwrap();
-                let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
-                return Json(contents).into_response();
-            }
-            Err(_) => {
-                if retried {
-                    return StatusCode::INTERNAL_SERVER_ERROR.into_response();
-                } else {
+        loop {
+            match tokio::time::timeout_at(
+                tokio::time::Instant::now() + std::time::Duration::from_secs(1),
+                closure(),
+            )
+            .await
+            {
+                Ok(r) => return Ok(r),
+                Err(_elapsed) => {
+                    if retried_times >= 1 {
+                        return Err(());
+                    }
+
                     shared_database.write().await.reconfigure().await;
-                    retried = true;
-                    continue;
+                    retried_times += 1;
                 }
             }
         }
     }
+
+    tracing::info!("Remote database ID: {}", shared_database.read().await.id);
+
+    let shapes = shared_database.read().await.collections.shapes.clone();
+
+    let docs = retry_maybe(|| Shape::all_async(&shapes), shared_database.clone())
+        .await
+        .expect("must connect")
+        .expect("must fetch");
+
+    let contents = docs.into_iter().map(|doc| doc.contents).collect::<Vec<_>>();
+    return Json(contents).into_response();
 
     // let shapes = state.remote_database.collections.shapes;
     // let docs = Shape::all_async(&shapes).await.unwrap();
