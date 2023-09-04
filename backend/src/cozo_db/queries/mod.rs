e7631a43c51d5bc4f024db82139a8c48f07881e7
phantie <funtom1605@gmail.com>
2023-09-04 21:42:25 +0300

    article queries

--- backend/src/cozo_db/queries/mod.rs
+++ backend/src/cozo_db/queries/mod.rs
@@ -88,3 +88,66 @@ pub fn ensure_articles_table(db: &DbInstance) -> OpResult {
     let result = db.run_script(script, Default::default(), ScriptMutability::Mutable);
     op_result(result)
 }
+
+#[derive(Default)]
+pub struct Article {
+    pub title: String,
+    pub public_id: String,
+    pub markdown: String,
+    pub draft: bool,
+}
+
+#[derive(Debug)]
+pub struct ArticleWithId {
+    pub id: String,
+    pub title: String,
+    pub public_id: String,
+    pub markdown: String,
+    pub draft: bool,
+}
+
+#[tracing::instrument(name = "Put article", skip_all)]
+pub fn put_article(db: &DbInstance, article: Article) -> OpResult {
+    let script = include_str!("articles/put.cozo");
+    let params: BTreeMap<String, DataValue> = map_macro::btree_map! {
+        "title".into() => article.title.into(),
+        "public_id".into() => article.public_id.into(),
+        "markdown".into() => article.markdown.into(),
+        "draft".into() => article.draft.into(),
+    };
+
+    let result = db.run_script(script, params, ScriptMutability::Mutable);
+    op_result(result)
+}
+
+#[tracing::instrument(name = "Find article by public_id", skip_all)]
+pub fn find_article_by_public_id(
+    db: &DbInstance,
+    public_id: &str,
+) -> Result<Option<ArticleWithId>> {
+    let script = include_str!("articles/find_by_public_id.cozo");
+    let params: BTreeMap<String, DataValue> = map_macro::btree_map! {
+        "public_id".into() => public_id.into()
+    };
+    let result = db
+        .run_script(script, params, ScriptMutability::Mutable)
+        .map_err(Error::EngineError)?;
+
+    let headers = result.headers.iter().map(String::as_str).collect_vec();
+    let rows = result.rows.iter().map(Vec::as_slice).collect_vec();
+
+    match (&headers[..], &rows[..]) {
+        (
+            ["id", "public_id", "title", "markdown", "draft"],
+            [[DataValue::Uuid(UuidWrapper(id)), DataValue::Str(public_id), DataValue::Str(title), DataValue::Str(markdown), DataValue::Bool(draft)]],
+        ) => Ok(Some(ArticleWithId {
+            id: id.to_string(),
+            title: title.to_string(),
+            public_id: public_id.to_string(),
+            markdown: markdown.to_string(),
+            draft: *draft,
+        })),
+        (["id", "public_id", "title", "markdown", "draft"], []) => Ok(None),
+        _ => Err(Error::ResultError(result)),
+    }
+}
