ef426738b4dbb34500657d48ca2029be6b8cbb07
phantie <funtom1605@gmail.com>
2023-09-05 17:17:23 +0300

    db articles tests

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -38,26 +38,12 @@ pub fn start_db(db: DbInstance) -> DbInstance {
     }
 
     {
-        // Articles
+        // Articles:
+        // Create missing tables: articles
         if q::ensure_articles_table(db).is_err() {
             let result = q::create_articles_table(db);
             assert!(result.is_ok());
         }
-
-        let article = interfacing::Article::default();
-        q::put_article(db, article.clone()).unwrap();
-
-        let article = q::find_article_by_public_id(db, "").unwrap().unwrap();
-        assert_eq!(article.public_id, "");
-
-        let article_id = article.id;
-        let mut article = interfacing::ArticleWithId::default();
-        article.id = article_id;
-        article.public_id = "updated".into();
-        q::update_article(db, article).unwrap();
-        let article = q::find_article_by_public_id(db, "updated").unwrap();
-        assert!(article.is_some());
-        assert_eq!(article.unwrap().public_id, "updated");
     }
 
     {
@@ -139,5 +125,47 @@ mod tests {
 
         assert_eq!(&user.username, &user_data.username);
         assert_eq!(&user.pwd_hash, &user_data.pwd_hash);
+    }
+
+    #[test]
+    fn articles_test() {
+        let db = &db();
+
+        assert_err!(q::ensure_articles_table(db));
+        assert_ok!(q::create_articles_table(db));
+        assert_ok!(q::ensure_articles_table(db));
+
+        let assert_article_count =
+            |count: usize| assert_eq!(q::find_articles(db).expect("op to succeed").len(), count);
+
+        assert_article_count(0);
+
+        let article_data = interfacing::Article::default();
+        assert_ok!(q::put_article(db, article_data.clone()));
+        assert_article_count(1);
+
+        let article = q::find_article_by_public_id(db, &article_data.public_id)
+            .expect("op to succeed")
+            .expect("to find the article");
+        assert_eq!(&article.public_id, &article_data.public_id);
+        assert_eq!(&article.title, &article_data.title);
+        assert_eq!(&article.markdown, &article_data.markdown);
+        assert_eq!(&article.draft, &article_data.draft);
+
+        let updated_article_data = interfacing::ArticleWithId {
+            id: article.id,
+            public_id: "updated".into(),
+            markdown: "updated".into(),
+            title: "updated".into(),
+            draft: false,
+        };
+
+        assert_ok!(q::update_article(db, updated_article_data.clone()));
+        assert_article_count(1);
+        let article = q::find_article_by_public_id(db, &updated_article_data.public_id)
+            .expect("op to succeed")
+            .expect("to find the article");
+
+        assert_eq!(&article, &updated_article_data);
     }
 }
